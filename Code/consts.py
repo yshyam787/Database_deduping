ed2f0cc1acd4fdcc559834c44204bcc189ea2f40
yshyam787 <yshyam787@gmail.com>
2021-07-30 00:36:09 +0200

    consts file

---
+++ Code/consts.py
@@ -0,0 +1,6 @@
+import os
+target_data = '/Users/shyamyadav/PycharmProjects/Git/yshyam787/Code/sample_database/'
+result_base = '/Users/shyamyadav/PycharmProjects/Git/yshyam787/Code/Results/'
+config_base = '/Users/shyamyadav/PycharmProjects/Git/yshyam787/Code/Model/'
+
+print(target_data, result_base, config_base)
